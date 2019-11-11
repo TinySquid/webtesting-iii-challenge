70f1e9bbcb5c6c429fee4ab0c863a1e660d13f31
Michael Nunes <miketnunes@gmail.com>
2019-11-11 13:10:35 -0800

    Add dashboard test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,31 @@
 // Test away
 /* spec
 - shows the controls and display
-- defaults to `unlocked` and `open`
-- cannot be closed or opened if it is locked
 */
+
+//Get our test library imported
+import React from 'react';
+import { cleanup, render } from 'react-testing-library';
+
+//Import the component to test
+import Dashboard from './Dashboard';
+
+//Unmount and cleanup dom after test is done.
+afterEach(cleanup);
+
+describe('<Dashboard />', () => {
+  it('renders controls and display component', () => {
+    // const { getByText } = render(<Dashboard />);
+    const wrapper = render(<Dashboard />);
+
+    // const displayDiv = wrapper.container.querySelector('.some-display'); //Sanity check, returns null since no element with class exists, fails test.
+    const displayDiv = wrapper.container.querySelector('.display');
+
+    expect(displayDiv).toBeTruthy();
+
+    // const controlsDiv = wrapper.container.querySelector('.wrong-controls'); //Sanity check
+    const controlsDiv = wrapper.container.querySelector('.controls');
+
+    expect(controlsDiv).toBeTruthy();
+  });
+});
