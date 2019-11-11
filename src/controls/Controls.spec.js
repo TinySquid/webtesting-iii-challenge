862f1eaee0f3c7a69830e5818cd20238377e749b
Michael Nunes <miketnunes@gmail.com>
2019-11-11 12:28:29 -0800

    Add another test, refactor first test and add sanity checks

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -19,17 +19,51 @@ afterEach(cleanup);
 
 describe('<Controls />', () => {
   it('has buttons for open/close & lock/unlock', () => {
-    const mockProps = {
-      locked: false,
-      closed: false,
-      toggleLocked: null,
-      toggleClosed: null
-    }
+    const mockProps = { locked: false, closed: false };
 
     const { getByText } = render(<Controls {...mockProps} />);
 
     // expect(getByText(/Unlock Gate/i)).toBeTruthy(); //Sanity check
-    expect(getByText(/Close Gate/i)).toBeTruthy();
-    expect(getByText(/Lock Gate/i)).toBeTruthy();
+    expect(getByText(/Close Gate/)).toBeTruthy();
+    expect(getByText(/Lock Gate/)).toBeTruthy();
+  });
+
+  it('sets correct button text and disabled state to reflect gate state', () => {
+    const mockPropsClosed = { locked: false, closed: true, };
+    const mockPropsOpen = { locked: false, closed: false };
+    const mockPropsLocked = { locked: true, closed: true };
+
+    //Render a default gate (open and unlocked)
+    const { getByText, rerender } = render(<Controls {...mockPropsOpen} />);
+
+    const toggleLock = getByText(/Lock Gate/);
+    const toggleGate = getByText(/Close Gate/);
+
+    expect(toggleLock.disabled).toBe(true);
+    expect(toggleGate.disabled).toBe(false);
+
+    expect(toggleLock.textContent).toMatch(/Lock Gate/)
+    expect(toggleGate.textContent).toMatch(/Close Gate/)
+
+    //Render a closed and unlocked gate.
+    rerender(<Controls {...mockPropsClosed} />);
+
+    // expect(toggleLock.disabled).toBe(true); //Sanity check
+    expect(toggleLock.disabled).toBe(false);
+    expect(toggleGate.disabled).toBe(false);
+
+    // expect(toggleLock.textContent).toMatch(/Unlock Gate/i); //Sanity check
+    expect(toggleLock.textContent).toMatch(/Lock Gate/)
+    expect(toggleGate.textContent).toMatch(/Open Gate/)
+
+    //Render a closed and locked gate.
+    rerender(<Controls {...mockPropsLocked} />);
+
+    expect(toggleLock.disabled).toBe(false);
+    expect(toggleGate.disabled).toBe(true);
+
+    // expect(toggleLock.textContent).toMatch(/Lock Gate/); //Sanity check
+    expect(toggleLock.textContent).toMatch(/Unlock Gate/);
+    expect(toggleGate.textContent).toMatch(/Open Gate/);
   });
 });
